64469aebfccf726ea9d26b8e3a302942133fdbcc
Joel Trinidad <jth_2929@hotmail.com>
2022-08-29 14:14:42 -0500

    feat: burger menu mobile and desktop added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
 import * as S from "./Navbar.styles"
 
 export default function Navbar() {
+  const [showMenuBurger, setshowMenuBurger] = useState(false)
+  
+  console.log(showMenuBurger);
   return (
-    <S.ContainerNavbar></S.ContainerNavbar>
+    <S.ContainerNavbar>
+      <S.ContainerLogo>
+        <S.ImageLogo src="/img/logo.png" alt="Logo Joel Trinidad" />
+        <div>
+          <h1>Joel Trinidad</h1>
+          <h2>Frontend Developer</h2>  
+        </div> 
+      </S.ContainerLogo>
+
+      <S.ContainerBurger onClick={() => setshowMenuBurger(!showMenuBurger)}>
+        <S.BurgerMenu/>
+        <S.BurgerMenu/>
+        <S.BurgerMenu/>
+      </S.ContainerBurger>
+
+      <S.ContainerItems showMenuBurger={showMenuBurger}>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.Button>Contact me</S.Button>
+      </S.ContainerItems>
+    </S.ContainerNavbar>
   )
 }
